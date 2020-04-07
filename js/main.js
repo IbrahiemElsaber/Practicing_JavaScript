5d9f00ccdc7f5bd87f18c6e46938bedb2c2c1dc5
IbrahiemElsaber <ibrahimelsaber8@gmail.com>
2020-04-08 00:54:36 +0200

    Using settimeout() and clearTimeout() on using functions

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@
 // document.getElementById("stopSetInterval").addEventListener("click",()=>{
 //     clearInterval(setTimeIntervalVar);
 // });
+//Using setTimeout() and clearTimeout() By functions
+// function setTimeOutFunc(){
+//     alert("This is a setTimeout inside function");
+// }
+// var setTimeOutVar = setTimeout(setTimeOutFunc, 2000);
+// document.getElementById("stopsetTimeOut").addEventListener("click",()=>{
+//     clearTimeout(setTimeOutVar);
+// });
