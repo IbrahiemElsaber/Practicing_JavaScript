5db6c86209c90fa04fe84389ece581aa7f101668
IbrahiemElsaber <ibrahimelsaber8@gmail.com>
2020-04-08 00:47:43 +0200

    Using setInterval() and clearInterval() on functions

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Using setInterval() and clearInterval on functions
+// function setTimeIntervalFunc(){
+//     alert("This is a setInterval function");
+// }
+// var setTimeIntervalVar = setInterval(setTimeIntervalFunc, 2000);
+// document.getElementById("stopSetInterval").addEventListener("click",()=>{
+//     clearInterval(setTimeIntervalVar);
+// });
