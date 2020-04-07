838309fd0ad5c7a96c3a46939ff3b511da071b65
IbrahiemElsaber <ibrahimelsaber8@gmail.com>
2020-04-08 01:07:51 +0200

    Using setInterval()/clearInterval()/setTimeout()/clearTimeout() inside class

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@
 // document.getElementById("stopsetTimeOut").addEventListener("click",()=>{
 //     clearTimeout(setTimeOutVar);
 // });
+class Timer{
+    constructor(){
+        //Using setInterval() and clearInterval() using class
+        // this.setTimeIntervalVar = setInterval(() => {
+        //     alert("This is a setInterval inside class ");
+        // }, 3000);
+
+        // document.getElementById("stopSetIntervalinsideclass").addEventListener("click",()=>{
+        //     clearInterval(this.setTimeIntervalVar);
+        // });
+        //UsingsetTimeout() and clearTimeout() inside class
+    //     this.setTimeOutVar = setTimeout(() => {
+    //         alert("this is a setTimeout() inside class ");
+    //     }, 2000);
+    //     document.getElementById("stopSetTimeOutinsideclass").addEventListener("click",()=>{
+    //         clearTimeout(this.setTimeOutVar);
+    //     });
+
+    // }
+    
+
+}
+onload = new Timer();
